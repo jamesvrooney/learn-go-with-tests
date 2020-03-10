f6cfbdee5db1d1a15a776843e16a1fbeabbfbf7e
jamesvrooney <jamesvrooney@hotmail.com>
2020-03-10 16:23:26 +0000

    Added tests for chapter 02

--- 01-hello-world/hello.go
+++ 01-hello-world/hello.go
@@ -2,10 +2,20 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
-    fmt.Println(Hello("James"))
+    fmt.Println(Hello("James", ""))
 }
 
-func Hello(name string) string {
-    return "Hello, " + name + "!"
+func Hello(name string, language string) string {
+    if name == "" {
+        name = "World"
+    }
+
+    if language == "Spanish" {
+        return "Hola, " + name
+    }
+
+    return englishHelloPrefix + name
 }
